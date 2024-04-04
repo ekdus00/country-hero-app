13d6d13606fcb214862db9c6e0792045ca38cc4e
wooxexn <jeonwooseon@naver.com>
2024-04-04 23:59:50 +0900

    스크랩, 이용안내, 자주하는질문 .java

---
+++ app/src/main/java/com/example/hero/ScrapActivity.java
@@ -0,0 +1,28 @@
+package com.example.hero;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+public class ScrapActivity extends AppCompatActivity {
+
+    // 뒤로가기 버튼
+    LinearLayout backBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // 뒤로가기 버튼을 클릭했을때 이벤트 등록
+        backBtn = findViewById(R.id.back_btn);
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+    }
+}
