ce8fef866e98a2a2d71db64555b58a2607858edc
wooxexn <jeonwooseon@naver.com>
2024-05-01 02:34:58 +0900

    이력서 관리화면 추가
    이력서 관리화면 관련 파일들 추가

---
+++ app/src/main/java/com/example/hero/ExperienceData.java
@@ -0,0 +1,13 @@
+package com.example.hero;
+
+public class ExperienceData {
+    private String name;
+
+    public ExperienceData(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
