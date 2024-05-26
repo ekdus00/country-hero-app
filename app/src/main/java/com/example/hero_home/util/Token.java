98cfc00a1502d994db5e0c4aca078a4edbefbddb
wooxexn <jeonwooseon@naver.com>
2024-05-27 00:18:16 +0900

    토큰 유틸 추가
    토큰을 한곳에서 관리하도록 함

---
+++ app/src/main/java/com/example/hero_home/util/Token.java
@@ -0,0 +1,5 @@
+package com.example.hero_home.util;
+
+public class Token {
+    public static String token = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0ZXN0ZXIxMDAiLCJleHAiOjE3MTY3MzUxMTgsImlzcyI6ImxvY2FsaG9zdDo4MDgwL2NvbnRyeUhlcm8iLCJ1c2VyVHlwZSI6IndvcmtlciJ9.Af1di1mWgrGoEfcE0KnZrG5bEaSAHM1LVog00TH0k38";
+}
