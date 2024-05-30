775f227d4b3b17d11d19aac2036af2ab3320d7c1
wooxexn <jeonwooseon@naver.com>
2024-05-31 00:23:28 +0900

    설정 화면 뒤로가기 텍스트 수정, 이용안내, 자주하는 질문 버튼 클릭 시 이동코드 추가
    설정 화면 뒤로가기 텍스트 수정, 이용안내, 자주하는 질문 버튼 클릭 시 이동코드 추가

--- app/src/main/java/com/example/hero/setting/activity/SettingActivity.java
+++ app/src/main/java/com/example/hero/setting/activity/SettingActivity.java
@@ -1,12 +1,17 @@
 package com.example.hero.setting.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.hero.faq.activity.FAQActivity;
+import com.example.hero.userguide.activity.UserGuideActivity;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.core.view.WindowCompat;
 import androidx.navigation.NavController;
@@ -18,11 +23,44 @@ import com.example.hero.databinding.ActivitySettingBinding;
 
 import com.example.hero.R;
 
+import org.w3c.dom.Text;
+
 public class SettingActivity extends AppCompatActivity {
+
+
+    LinearLayout backBtn;
+    TextView userGuideBtn;
+    TextView faqBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+
+        backBtn = findViewById(R.id.back_btn);
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+        userGuideBtn = findViewById(R.id.user_guide_btn);
+        userGuideBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SettingActivity.this, UserGuideActivity.class);
+                startActivity(intent);
+            }
+        });
+        faqBtn = findViewById(R.id.faq_btn);
+        faqBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SettingActivity.this, FAQActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
